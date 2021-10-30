43db5a3f7879fc2a7b264a99d16b3b244981ab13
mr-mohit <44066175+mr-mohit@users.noreply.github.com>
2021-10-30 17:05:13 -0400

    Connected to MongoDB

---
+++ config/db.js
@@ -0,0 +1,10 @@
+
+/*File Name - db.js
+Student Name - Mohit Verma
+Student ID - 301170872
+Web App Name- comp229-mohitVerma-middterm*/
+
+module.exports = {
+  AtlasDB:
+    "mongodb+srv://mohit:KAzUrLhdMiB5QVa4@cluster0.boymd.mongodb.net/midterm?retryWrites=true&w=majority",
+};
